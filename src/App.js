dfa8f51cf8bf171242d7ba6a09c2dc64f02512b2
kochenov <kochenof@gmail.com>
2022-03-10 12:54:11 +0300

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
 import Message from "./Message";
 import "./message.scss";
 
 function App() {
-  const textMessage = "This my message";
+  const [messages, setMessage] = useState([]);
+
+  const createMessage = (newMessage) => {
+    setMessage([...messages, newMessage]);
+  };
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      let lastMsg = messages[messages.length - 1];
+      let robotMsg = {
+        id: Date.now(),
+        author: "Робот Вася",
+        textMessage:
+          "Здравствуйте, " +
+          lastMsg.author +
+          ". Ваше сообщение принято к рассмотрению! Ожидайте ответа специалиста.",
+      };
+      if (lastMsg.author !== "Робот Вася") {
+        setTimeout(() => {
+          setMessage([...messages, robotMsg]);
+        }, 1500);
+      }
+    }
+  }, [messages]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Message text={textMessage} />
+      <header>
+        <div className="App-header">
+          {messages.map((message) => (
+            <Message message={message} key={message.id} />
+          ))}
+        </div>
       </header>
+      <Form create={createMessage} />
     </div>
   );
 }
