5dea3d145b0e8ed61dd3143db9f1face53b0595c
kochenov <kochenof@gmail.com>
2022-04-03 20:50:47 +0300

    Добавил список чатов

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Form from "./Form";
+import ListChats from "./ListChats";
 import Message from "./Message";
 import "./message.scss";
 
@@ -36,13 +37,14 @@ function App() {
 
   return (
     <div className="App">
-      <header>
+      <div className="row">
         <div className="App-header">
           {messages.map((message) => (
             <Message message={message} key={message.id} />
           ))}
         </div>
-      </header>
+        <ListChats />
+      </div>
       <Form create={createMessage} />
     </div>
   );
