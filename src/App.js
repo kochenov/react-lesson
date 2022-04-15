ad4cb7e3fd72cb5738f17b6b9416749cdcf59cc0
kochenov <kochenof@gmail.com>
2022-04-15 20:39:36 +0300

    Установил Redux, подключил к странице профиля

--- src/App.js
+++ src/App.js
@@ -7,36 +7,45 @@ import Chats from "./pages/Chats";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./components/NotFound";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
-
   const liders = [
-    { id: '1', name: "Владимир Путин" },
-    { id: '2', name: "Джозеф Байден" },
-    { id: '3', name: "Володимир Зеленский" },
-    { id: '4', name: "Олаф Шольц" },
-    { id: '5', name: "Эмманюэль Макрон" },
-    { id: '6', name: "Си Цзиньпин" },
+    { id: "1", name: "Владимир Путин" },
+    { id: "2", name: "Джозеф Байден" },
+    { id: "3", name: "Володимир Зеленский" },
+    { id: "4", name: "Олаф Шольц" },
+    { id: "5", name: "Эмманюэль Макрон" },
+    { id: "6", name: "Си Цзиньпин" },
   ];
 
   return (
-    <BrowserRouter>
-      <div className="container">
-        <div className="App">
-          <div className="row">
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/chats" element={<Chats liders={liders} list={true}/>}>
-                <Route path=":idChat" element={<Chats liders={liders} list={false}/>} />
-              </Route>
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <ListChats liders={liders}/>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="container">
+          <div className="App">
+            <div className="row">
+              <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route
+                  path="/chats"
+                  element={<Chats liders={liders} list={true} />}
+                >
+                  <Route
+                    path=":idChat"
+                    element={<Chats liders={liders} list={false} />}
+                  />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+              <ListChats liders={liders} />
+            </div>
           </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import store from "../store";
+import { changleVisible } from "../store/profile/actions";
 
 const Profile = () => {
-  return (
-    <div className='Home'>
-      <h1>Профиль</h1>
-    </div>
-  )
-}
+  const { showName, name } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-export default Profile
+  const setShowName = useCallback(() => {
+    dispatch(changleVisible);
+  }, [dispatch]);
+  return (
+    <div className="Home">
+      <h1>Профиль</h1>
+      <input
+        type="checkbox"
+        checked={showName}
+        value={showName}
+        onChange={setShowName}
+      />
+      <span>Показать имя</span>
+
+
+      {showName && <h3>{name}</h3>}
+    </div>
+  );
+};
+
+export default Profile;
